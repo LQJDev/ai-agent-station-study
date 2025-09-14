c16cc7788bbd4f81a9df102c521eafba8a92e093
wssblqj <1340935597@qq.com>
2025-09-14 14:18:54 +0800

    feat: 初始化代码

---
+++ ai-agent-station-study-app/src/main/java/com/lqj/ai/Application.java
@@ -0,0 +1,19 @@
+package com.lqj.ai;
+
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author 李岐鉴
+ * @Date 2025/9/14
+ * @Description Application 类
+ */
+@SpringBootApplication
+@Configurable
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
